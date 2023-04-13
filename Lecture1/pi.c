5aeb654dd0e914c2dbc843a443ad8d8fd1cfeb7b
Takeshi Kawasaki <72239760+TakeshiKawasaki@users.noreply.github.com>
2023-04-13 09:44:14 +0900

    Update pi.c

--- Lecture1/pi.c
+++ Lecture1/pi.c
@@ -26,7 +26,7 @@ int main(void){
     file.close();
     pi = (double)count / max * 4;
     printf("%.20f\n",pi); //by C, %.20f -- Displaying with 20 decimal precision                                                                                             
-    std:cout<< setprecision(21)  <<  pi  << std:endl; // by C++                                                                                                                
+    std:cout<< std::setprecision(21) <<  pi  << std:endl; // by C++                                                                                                                
  
     return 0;
 }
