4fab56fc21e4d29b6dd1ca4a3a92aed4ca4b4e15
Takeshi Kawasaki <72239760+TakeshiKawasaki@users.noreply.github.com>
2023-05-14 14:30:59 +0900

    Update analyze.cpp

--- Lecture5/analyze.cpp
+++ Lecture5/analyze.cpp
@@ -6,7 +6,7 @@
 #include <cfloat>
 #include "BM.h"
 
-#define temp 0.01
+#define temp 1.0
 #define dt 0.01
 #define ensemble 10000
 #define window 39
@@ -40,7 +40,7 @@ void input(double (*x)[dim],double (*v)[dim],double *t){
   file.close();
 }
 
-void output(double *t,double *dr2,double *corr,double integral){
+void output(double *t,double *dr2,double *corr){
   char filename[128];
   std::ofstream file;
   sprintf(filename,"msd_dt%.3fT%.3f.dat",dt,temp);
@@ -54,11 +54,9 @@ void output(double *t,double *dr2,double *corr,double integral){
   for(int i=1;i<window;i++)
     file<<t[i]-t[0]<<"\t"<<corr[i]<<std::endl;
   file.close();
-
-  std::cout<<"A result of the integral ="<<integral<<std::endl;
 }
 
-void analyze(double (*x)[dim],double (*v)[dim],double *t,double *dr2,double *corr,double *integral){
+void analyze(double (*x)[dim],double (*v)[dim],double *t,double *dr2,double *corr){
   double dx[dim],corr_x[dim];
   for(int i=0;i<ensemble;i++)
     for(int j=0;j<window;j++){
@@ -67,22 +65,21 @@ void analyze(double (*x)[dim],double (*v)[dim],double *t,double *dr2,double *cor
 	corr_x[k]=v[j+window*i][k]*v[window*i][k];
 	dr2[j]+=(dx[k]*dx[k])/ensemble;
 	corr[j]+=(corr_x[k])/ensemble;
-	if(j!=window-1)
-	  *integral+=0.5*(v[j+window*i][k]*v[window*i][k]+v[j+1+window*i][k]*v[window*i][k])*(t[j+1]-t[j])/ensemble;
+
       }
     }
 }
 
 int main(){
-  double t[ensemble*window],dr2[window],corr[window],integral=0.0;
+  double t[ensemble*window],dr2[window],corr[window];
   int i,j;
   double  (*x)[dim] = new double[ensemble*window][dim];
   double  (*v)[dim] = new double[ensemble*window][dim];
 
   ini(dr2,corr);
   input(x,v,t);
-  analyze(x,v,t,dr2,corr,&integral);
-  output(t,dr2,corr,integral);
+  analyze(x,v,t,dr2,corr);
+  output(t,dr2,corr);
   delete[] x;
   delete[] v;
   return 0;
